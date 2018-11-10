5f4c5ba2c5511a85164b4d618923104ea4ddfb54
Eduardo Nunes <earaujo@gmail.com>
2018-11-10 20:15:38 -0200

    Adding map title

--- app/src/main/java/com/earaujo/usinglivedata/MainActivity.kt
+++ app/src/main/java/com/earaujo/usinglivedata/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.earaujo.usinglivedata
 
+import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.Transformations
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var adapter: RedditAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
+    private val searchRequest = MediatorLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +26,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onResume() {
         super.onResume()
+        installSearch()
         btn_search.setOnClickListener(this)
     }
 
+    var searchTransform = Transformations.switchMap(searchRequest) {
+        ApiUtil.searchReddit(it, 20)
+    }
+
+    var titleObserver = Transformations.map(searchTransform) {
+        it.data?.children?.get(0)?.data?.title
+    }
+
     fun performSearch(search: String) {
-        ApiUtil.searchReddit(search, 20).observe(this, Observer { searchResult ->
+        this.searchRequest.value = search
+    }
+
+    fun installSearch() {
+        /*searchTransform.observe(this, Observer { searchResult ->
             if (searchResult != null) {
                 setupRecyclerView(searchResult.data?.children!!)
+            }
+        })*/
+
+        titleObserver.observe(this, Observer { title ->
+            if (title != null) {
+                tv_title.text = title
+                tv_title.visibility = View.VISIBLE
             }
         })
     }
